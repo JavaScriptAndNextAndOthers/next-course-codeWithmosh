a3103fe0be648cb3ff660fe9e4e74b60a9d63022
Agyesam <Agyesam2@hotmail.com>
2024-08-11 10:07:07 +0000

    Update route.tsx

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -2,20 +2,28 @@ import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 import { prisma } from "@/prisma/client";
 
+
+// GET /api/products
 export async function GET(request: NextRequest) {
   const users = await prisma.user.findMany();
 
   return NextResponse.json(users);
 }
 
+// POST /api/users
 export async function POST(request: NextRequest) {
+  
+  // Parse the request body
   const body = await request.json();
+  
+  // Validate the request body
   const validation = schema.safeParse(body);
   if (!validation.success)
     return NextResponse.json(validation.error.errors, {
       status: 400,
     });
 
+    // Check if the user already exists
   const user = await prisma.user.findUnique({
     where: { email: body.email },
   });
@@ -26,6 +34,7 @@ export async function POST(request: NextRequest) {
       { status: 400 }
     );
 
+    // Create the user
   const newUser = await prisma.user.create({
     data: {
       name: body.name,
